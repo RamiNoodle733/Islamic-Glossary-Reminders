223da419ae90df04825dd658b4c6db7220b8baa6
Rami Razaq <ramirazaq@gmail.com>
2024-08-14 19:18:00 -0500

    Updated server.js to use dynamic port for Render

--- server.js
+++ server.js
@@ -161,6 +161,7 @@ app.get('/can-check-in', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
